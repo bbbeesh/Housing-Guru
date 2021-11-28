642b08ee78843d5efc0a50f269b01d35a43ca4f6
bbbeesh <72682741+bbbeesh@users.noreply.github.com>
2021-11-28 19:40:27 +0530

    First Update

---
+++ src/pages/Rooms.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Rooms extends Component {
+	render() {
+		return <div>Hello from Rooms Page!</div>;
+	}
+}
---
+++ src/pages/SingleRooms.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class SingleRooms extends Component {
+	render() {
+		return <div>Hello from Single Rooms Page!</div>;
+	}
+}
